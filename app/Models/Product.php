d77f1b7bb44234f509b4b47e4b227b3435c4f9f1
Harshadeva <harshadeva@webmotech.com>
2025-11-20 06:07:28 +0530

    feat: update model relationships

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    //
+    protected $guarded = [];
+
+    public function orderItems()
+    {
+        return $this->hasMany(OrderItem::class);
+    }
+
+    public function stockReservations()
+    {
+        return $this->hasMany(StockReservation::class);
+    }
 }
