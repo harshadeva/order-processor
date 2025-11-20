cdd6273b6daf96ec916c415f193f194c791daddd
Harshadeva <harshadeva@webmotech.com>
2025-11-20 21:01:39 +0530

    WIP: partial implementation of order import

--- app/Jobs/ProcessOrderJob.php
+++ app/Jobs/ProcessOrderJob.php
@@ -2,26 +2,46 @@
 
 namespace App\Jobs;
 
+use App\Models\Order;
+use App\Models\OrderItem;
 use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Foundation\Queue\Queueable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
 
 class ProcessOrderJob implements ShouldQueue
 {
-    use Queueable;
+    use Dispatchable, Queueable, InteractsWithQueue, SerializesModels;
+
+    public $data;
 
     /**
      * Create a new job instance.
      */
-    public function __construct()
+    public function __construct(array $data)
     {
-        //
+        $this->data = $data;
     }
+
 
     /**
      * Execute the job.
      */
     public function handle(): void
     {
-        //
+        DB::transaction(function () {
+
+            $order = Order::firstOrCreate(
+                ['code' => $this->data['order_code']],
+                [
+                    'customer_id' => $this->data['customer_id'],
+                    'total' => $this->data['total']
+                ]
+            );
+        });
+
     }
 }
